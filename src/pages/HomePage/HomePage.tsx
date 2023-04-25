98ec964c39c636b95d50dc0d9818328e7cd054f7
firuzeabdullazade <firuzeabdullazade64@gmail.com>
2023-04-25 17:46:41 +0400

    Homepage was rewritten

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -7,13 +7,209 @@ import { HowItWorks } from "./components/HowItWorks/HowItWorks";
 import { SubscribeWidget } from "./components/SubscribeWidget/SubscribeWidget";
 import { TopRatedArtists } from "./components/TopRatedArtists/TopRatedArtists";
 import { TrendingCollection } from "./components/TrendingCollection/TrendingCollection";
+
+import rocket from "./assets/RocketLaunch (1).svg";
+import keepitreal from "./assets/Avatar Placeholder (1).png";
+import digiLab from "./assets/Avatar Placeholder (5).png";
+import gravityOne from "./assets/Avatar Placeholder (6).png";
+import juanie from "./assets/Avatar Placeholder (7).png";
+import blueWhale from "./assets/Avatar Placeholder (8).png";
+import mrFox from "./assets/Avatar Placeholder (9).png";
+import shroomie from "./assets/Avatar Placeholder (10).png";
+import robotica from "./assets/Avatar Placeholder (11).png";
+import rustyRobot from "./assets/Avatar Placeholder (12).png";
+import animakid from "./assets/Avatar Placeholder (13).png";
+import dotgu from "./assets/Avatar Placeholder (14).png";
+import ghiblier from "./assets/Avatar Placeholder (15).png";
+
+import art from "./assets/Category Icon.png";
+import collectibles from "./assets/Photo & Icon.png";
+import music from "./assets/Photo & Icon (1).png";
+import photography from "./assets/Photo & Icon (2).png";
+import video from "./assets/Category Icon (1).png";
+import utility from "./assets/Category Icon (2).png";
+import sport from "./assets/Category Icon (3).png";
+import virtualWorlds from "./assets/Category Icon (4).png";
+
 export const HomePage = () => {
+  const artists = [
+    {
+      rankingNumber: 1,
+      imageSrc: keepitreal,
+      name: "Keepitreal",
+      mobile: true,
+    },
+
+    {
+      rankingNumber: 2,
+      imageSrc: digiLab,
+      name: "DigiLab",
+      mobile: true,
+    },
+
+    {
+      rankingNumber: 3,
+      imageSrc: gravityOne,
+      name: "GravityOne",
+      mobile: true,
+    },
+
+    {
+      rankingNumber: 4,
+      imageSrc: juanie,
+      name: "Juanie",
+      mobile: true,
+    },
+
+    {
+      rankingNumber: 5,
+      imageSrc: blueWhale,
+      name: "BlueWhale",
+      mobile: true,
+    },
+
+    {
+      rankingNumber: 6,
+      imageSrc: mrFox,
+      name: "Mr Fox",
+      mobile: false,
+    },
+
+    {
+      rankingNumber: 7,
+      imageSrc: shroomie,
+      name: "Shroomie",
+      mobile: false,
+    },
+
+    {
+      rankingNumber: 8,
+      imageSrc: robotica,
+      name: "Robotica",
+      mobile: false,
+    },
+
+    {
+      rankingNumber: 9,
+      imageSrc: rustyRobot,
+      name: "RustyRobot",
+      mobile: false,
+    },
+
+    {
+      rankingNumber: 10,
+      imageSrc: animakid,
+      name: "Animakid",
+      mobile: false,
+    },
+
+    {
+      rankingNumber: 11,
+      imageSrc: dotgu,
+      name: "Dotgu",
+      mobile: false,
+    },
+
+    {
+      rankingNumber: 12,
+      imageSrc: ghiblier,
+      name: "Ghiblier",
+      mobile: false,
+    },
+  ];
+
+  const categories = [
+    {
+      imageSrc: art,
+      name: "Art",
+      bottom: false,
+    },
+
+    {
+      imageSrc: collectibles,
+      name: "Collectibles",
+      bottom: false,
+    },
+
+    {
+      imageSrc: music,
+      name: "Music",
+      bottom: false,
+    },
+
+    {
+      imageSrc: photography,
+      name: "Photography",
+      bottom: false,
+    },
+
+    {
+      imageSrc: video,
+      name: "Video",
+      bottom: true,
+    },
+
+    {
+      imageSrc: utility,
+      name: "Utility",
+      bottom: true,
+    },
+
+    {
+      imageSrc: sport,
+      name: "Sport",
+      bottom: true,
+    },
+
+    {
+      imageSrc: virtualWorlds,
+      name: "Virtual Worlds",
+      bottom: true,
+    },
+  ];
+
   return (
     <>
       <HeroPart />
       <TrendingCollection />
-      <TopRatedArtists />
-      <BrowseCategories />
+      <section className="top-rated-artists">
+        <div className="section-headline-2">
+          <div className="subheadline">
+            <h3>Top Creators</h3>
+            <div className="subhead">
+              Checkout Top Rated Creators on the NFT Marketplace
+            </div>
+          </div>
+          <button>
+            <img src={rocket} alt="Rocket Launch" />
+            View Rankings
+          </button>
+        </div>
+        <div className="artist-cards-grid">
+          {artists.map((artist) => (
+            <TopRatedArtists
+              rankingNumber={artist.rankingNumber}
+              imageSrc={artist.imageSrc}
+              name={artist.name}
+              mobile={artist.mobile}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="browse-categories">
+        <div className="section-headline">
+          <h3>Browse Categories</h3>
+        </div>
+        <div className="category-cards-grid">
+          {categories.map((category) => (
+            <BrowseCategories
+              imageSrc={category.imageSrc}
+              name={category.name}
+              bottom={category.bottom}
+            />
+          ))}
+        </div>
+      </section>
       <Discover />
       <Highlight />
       <HowItWorks />
--- src/pages/HomePage/components/TopRatedArtists/TopRatedArtists.tsx
+++ src/pages/HomePage/components/TopRatedArtists/TopRatedArtists.tsx
@@ -1,206 +1,29 @@
 import "./TopRatedArtists.scss";
-import rocket from './RocketLaunch (1).svg';
-import keepitreal from './Avatar Placeholder (1).png';
-import digiLab from './Avatar Placeholder (5).png';
-import gravityOne from './Avatar Placeholder (6).png';
-import juanie from './Avatar Placeholder (7).png';
-import blueWhale from './Avatar Placeholder (8).png';
-import mrFox from './Avatar Placeholder (9).png';
-import shroomie from './Avatar Placeholder (10).png';
-import robotica from './Avatar Placeholder (11).png';
-import rustyRobot from './Avatar Placeholder (12).png';
-import animakid from './Avatar Placeholder (13).png';
-import dotgu from './Avatar Placeholder (14).png';
-import ghiblier from './Avatar Placeholder (15).png';
 
-
-export const TopRatedArtists = () => {
-    return (
-        <>
-        <section className="top-rated-artists">
-        <div className="section-headline-2">
-          <div className="subheadline">
-            <h3>Top Creators</h3>
-            <div className="subhead">
-              Checkout Top Rated Creators on the NFT Marketplace
-            </div>
-          </div>
-          <button>
-            <img src= {rocket} alt="Rocket Launch" />
-            View Rankings
-          </button>
-        </div>
-        <div className="artist-cards-grid">
-          <div className="artist-cards-row-mobile">
-            <div className="artist-card-mobile">
-              <div className="ranking-number">1</div>
-              <div className="artist-avatar">
-                <img src= {keepitreal} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>Keepitreal</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-            <div className="artist-card-mobile">
-              <div className="ranking-number">2</div>
-              <div className="artist-avatar">
-                <img src= {digiLab} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>DigiLab</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-            <div className="artist-card-mobile">
-              <div className="ranking-number">3</div>
-              <div className="artist-avatar">
-                <img src= {gravityOne} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>GravityOne</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-            <div className="artist-card-mobile">
-              <div className="ranking-number">4</div>
-              <div className="artist-avatar">
-                <img src= {juanie} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>Juanie</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="artist-cards-row-mobile">
-            <div className="artist-card-mobile">
-              <div className="ranking-number">5</div>
-              <div className="artist-avatar">
-                <img src= {blueWhale} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>BlueWhale</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-            <div className="artist-card">
-              <div className="ranking-number">6</div>
-              <div className="artist-avatar">
-                <img src= {mrFox} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>Mr Fox</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-            <div className="artist-card">
-              <div className="ranking-number">7</div>
-              <div className="artist-avatar">
-                <img src= {shroomie} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>Shroomie</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-            <div className="artist-card">
-              <div className="ranking-number">8</div>
-              <div className="artist-avatar">
-                <img src= {robotica} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>Robotica</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="artist-cards-row">
-            <div className="artist-card">
-              <div className="ranking-number">9</div>
-              <div className="artist-avatar">
-                <img src= {rustyRobot} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>RustyRobot</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-            <div className="artist-card">
-              <div className="ranking-number">10</div>
-              <div className="artist-avatar">
-                <img src= {animakid} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>Animakid</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-            <div className="artist-card">
-              <div className="ranking-number">11</div>
-              <div className="artist-avatar">
-                <img src= {dotgu} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>Dotgu</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-            <div className="artist-card">
-              <div className="ranking-number">12</div>
-              <div className="artist-avatar">
-                <img src= {ghiblier} alt="Avatar" />
-              </div>
-              <div className="artist-info">
-                <h5>Ghiblier</h5>
-                <div className="additional-info">
-                  <div className="total-sale">Total Sale</div>
-                  <div className="eth">34.53 ETH</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mobile">
-          <button>
-            <img src= {rocket} alt="Rocket Launch" />
-            View Rankings
-          </button>
-        </div>
-      </section>
-        </>
-    )
+interface TopRatedArtistsProps {
+  rankingNumber: number;
+  imageSrc: string;
+  name: string;
+  mobile: boolean;
 }
+
+export const TopRatedArtists = (props: TopRatedArtistsProps) => {
+  const { rankingNumber, imageSrc, name, mobile } = props;
+  const className = `artist-card ${mobile && '-mobile'}`;
+
+  return (
+    <div className={className}>
+      <div className="ranking-number">{rankingNumber}</div>
+      <div className="artist-avatar">
+        <img src={imageSrc} alt="Avatar" />
+      </div>
+      <div className="artist-info">
+        <h5>{name}</h5>
+        <div className="additional-info">
+          <div className="total-sale">Total Sale</div>
+          <div className="eth">34.53 ETH</div>
+        </div>
+      </div>
+    </div>
+  );
+};
